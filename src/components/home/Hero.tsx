8eb6ae370fbfe43abaf5843980ae98c4bd1b806f
ziad <ziadabdelaziz1995@gmail.com>
2024-08-18 02:03:26 +0300

    feat: adjust stacking context on NavBar for adding blur effect and adjusting responsiveness in hero section

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -3,12 +3,12 @@ import heroImage from '@/assets/hero.png';
 const Hero = () => {
     return (
         <div className="relative w-full -z-10">
-            <img src={ heroImage } alt="hero" className='w-full h-[40vh] sm:h-auto object-cover z-0'/>
-            <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 w-full max-w-sm sm:max-w-xl md:max-w-3xl mt-3 sm:m-0'>
-                <div className='bg-app-secondary bg-opacity-60 text-white text-center px-8 sm:px-16 py-5'>
-                    <span className='font-bold text-2xl sm:text-3xl md:text-4xl'>THE FURNITURE YOU WANT</span>
+            <img src={ heroImage } alt="hero" className='w-full h-[50vh] sm:h-auto object-cover z-0'/>
+            <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 w-full max-w-sm sm:max-w-xl md:max-w-3xl lg:max-w-4xl mt-3 sm:m-0'>
+                <div className='backdrop-blur-sm bg-app-secondary/60 text-white text-center px-8 sm:px-16 py-5 rounded-md'>
+                    <span className='font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl'>THE FURNITURE YOU WANT</span>
                     <br />
-                    <span className='inline-block text-sm sm:text-xl w-4/5 md:w-3/6'>let Damietta's legacy of artistry transform your home.</span>
+                    <span className='inline-block mt-2 text-sm sm:text-xl lg:text-3xl w-4/5 md:w-3/6 lg:w-4/5'>let Damietta's legacy of artistry transform your home.</span>
                 </div>
             </div>
         </div>
--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -6,10 +6,11 @@ import NavMenu from './NavMenu';
 
 const NavBar = () => {
     const [isOpenMenu, setIsOpenMenu] = useState(false);
+
     return ( 
-        <div className="fixed w-full sm:py-2 pb-0 bg-transparet shadow-md z-10">
-            <nav className='flex justify-between'>
-                <div className="ml-2 my-2 pb-0.5 sm:my-auto inline-block">
+        <div className="fixed w-full pb-0 bg-transparet shadow-md z-20">
+            <nav className='flex justify-between py-1 sm:py-4 backdrop-blur-sm'>
+                <div className="ml-2 my-2 pb-0.5 text-xl sm:my-auto inline-block">
                     Damasya
                 </div>
                 <ul className="hidden sm:inline">
@@ -30,11 +31,13 @@ const NavBar = () => {
                         <span className='bg-app-action'></span>
                     </li>
                 </ul>
-                <div onClick={()=>setIsOpenMenu(!isOpenMenu)} className={`fixed right-1 inline sm:hidden ${isOpenMenu && 'text-white'} scale-75 z-50`}>
-                    <Hamburger />
-                </div>
-                <NavMenu showMenu={isOpenMenu} />
             </nav>
+            <div onClick={()=>setIsOpenMenu(!isOpenMenu)} 
+                    className={`fixed right-1 top-1 inline sm:hidden ${isOpenMenu? 'text-white' : ''} scale-75 z-50`}
+            >
+                <Hamburger />
+            </div>
+            <NavMenu showMenu={isOpenMenu} />
         </div>
      );
 }
