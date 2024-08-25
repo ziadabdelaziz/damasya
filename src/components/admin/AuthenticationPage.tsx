307595748d173cb89d5c8a6dfeb8b875d70cfcc3
ziad <ziadabdelaziz1995@gmail.com>
2024-08-25 22:42:06 +0300

    feat:admin auth basic UI

--- src/components/admin/AuthenticationPage.tsx
+++ src/components/admin/AuthenticationPage.tsx
@@ -1,12 +1,13 @@
 const AuthenticationPage = () => {
     return (
-        <div>
-            <form>
-                <h1 className="p-20">Admin</h1>
+        <div className="grid place-items-center h-screen">
+            <form className="bg-app-secondary/50 w-2/3 md:w-3/5 lg:w-2/5 p-10 rounded-lg">
+                <h1 className="pb-10">Admin</h1>
                 <label htmlFor="email">Email</label>
-                <input type="email" id="email"/>
+                <input className="block w-full rounded-md bg-gray-100 h-10 px-2 focus:border-app-secondary focus:border-2 outline-none" type="email" id="email"/>
+                <br />
                 <label htmlFor="password">Password</label>
-                <input type="password" />
+                <input className="block w-full rounded-md bg-gray-100 h-10 px-2 focus:border-app-secondary focus:border-2 outline-none" type="password" />
             </form>
         </div>
     );
